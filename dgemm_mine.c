70cede3199b137f80d20cbdf62a8eaf672ea8930
Jihoon <Jaykim9206@github.com>
2020-10-02 22:56:01 +0000

    try hard with GCC_ALiGN

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,8 +1,14 @@
 const char* dgemm_desc = "My awesome dgemm.";
 
-void square_dgemm(const int M, const double *A, const double *B, double *C)
+#define GCC_ALN(var,alignment)\
+	__builtin_assume_aligned(var,alignment)
+#define MEMBD ((int) 64)
+void square_dgemm(const int M, const double* restrict A, const double* restrict B, double* restrict C)
 {
     int i, j, k;
+   A= GCC_ALN(A,MEMBD);
+   B= GCC_ALN(B,MEMBD);
+   C= GCC_ALN(C,MEMBD);
     for (j = 0; j < M; ++j) {
         for (k = 0; k < M; ++k) {
             for (i = 0; i < M; ++i){
