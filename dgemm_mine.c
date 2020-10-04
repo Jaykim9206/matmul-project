bf289ea0e360daf78ba570afe1e20527c327c5be
Jihoon <Jaykim9206@github.com>
2020-10-04 20:05:46 +0000

    alignd memory

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 const char* dgemm_desc = "My awesome dgemm.";
 
 #define GCC_ALN(var,alignment)\
@@ -6,15 +7,38 @@ const char* dgemm_desc = "My awesome dgemm.";
 void square_dgemm(const int M, const double* restrict A, const double* restrict B, double* restrict C)
 //void square_dgemm(const int M, const double *A, const double *B, double *C)
 {
+	double* restrict newA =  (double*) aligned_alloc(MEMBD,M*M*sizeof(double));
+	double* restrict newB =  (double*) aligned_alloc(MEMBD,M*M*sizeof(double));
+	double* restrict newC =  (double*) aligned_alloc(MEMBD,M*M*sizeof(double));
+	 newA= GCC_ALN(newA,MEMBD);
+	 newB= GCC_ALN(newB,MEMBD);
+   	 newC= GCC_ALN(newC,MEMBD);
+
+
     int i, j, k;
-   A= GCC_ALN(A,MEMBD);
-   B= GCC_ALN(B,MEMBD);
-   C= GCC_ALN(C,MEMBD);
+  
+    for (j = 0; j < M; ++j) {
+            for (i = 0; i < M; ++i){
+
+		newA[j*M+i] = A[j*M+i];
+		newB[j*M+i] = B[j*M+i];
+                newC[j*M+i] = C[j*M+i];
+	    }
+    }
     for (j = 0; j < M; ++j) {
         for (k = 0; k < M; ++k) {
             for (i = 0; i < M; ++i){
-                C[j*M+i] += A[k*M+i] * B[j*M+k];
+                newC[j*M+i] += newA[k*M+i] * newB[j*M+k];
 	    }
         }
     }
+ 
+    for (j = 0; j < M; ++j) {
+            for (i = 0; i < M; ++i){
+                C[j*M+i] += newC[j*M+i];
+	    }
+    }
+    free(newA);
+    free(newB);
+    free(newC);
 }
